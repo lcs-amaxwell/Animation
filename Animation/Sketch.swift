c48cbf6a6a553ebc01a3b2cfcbd4aa057749d1f4
Maxwell Anna <amaxwell@lcs-mba3091.lcs.on.ca>
2017-11-02 13:38:34 -0400

    Final Pumpkin

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,9 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    // Position of circle
+    var y : Int
+   
     // This function runs once
     override init() {
         
@@ -18,13 +21,23 @@ class Sketch : NSObject {
         // Set starting position
         x = 0
         
+        // Set starting vertical position
+        y = 0
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
+        // Move from left to right
         x += 1
+        
+        // Determine the vertical position using the sine function
+        let xInRadians = Double(x)*Double(Double.pi)/180.0
+        y = Int(100*sin(xInRadians)+300)
+        
+        // Clear the canvas
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 800, height: 600)
         
         // Clear the canvas
         canvas.fillColor = Color.white
@@ -34,40 +47,50 @@ class Sketch : NSObject {
         canvas.drawShapesWithBorders = false
         
         // Move the origin to the middle of the canvasand
-        canvas.translate(byX: x, byY: 300)
+        canvas.translate(byX: x, byY: y)
         
-        // Draw a bat relative to the origin
-        canvas.fillColor = Color.black
+        // Draw pumpkin relative to the origin
+        canvas.fillColor = Color.orange
         
         // First make an ellipse to form the body
-        canvas.drawEllipse(centreX: 0, centreY: 0, width: 200, height: 100)
+        canvas.drawEllipse(centreX: 0, centreY: 0, width: 100, height: 100)
+        canvas.drawEllipse(centreX: 40, centreY: 0, width: 100, height: 100)
+        
+        // Make stem black
+        canvas.fillColor = Color.black
+        
+        // Draw the stem
+        canvas.drawEllipse(centreX: 20, centreY: 62, width: 30, height: 30)
+        
+        // Make stem properly shaped
+        canvas.fillColor = Color.white
+        
+        // Make stem properly shaped
+        canvas.drawEllipse(centreX: 30, centreY: 65, width: 30, height: 30)
+        
+        // Make the eyes yellow
+        canvas.fillColor = Color.yellow
+        
+        // Draw the eyes
+        canvas.drawRectangle(bottomLeftX: -20, bottomLeftY: 5, width: 20, height: 20)
+        canvas.drawRectangle(bottomLeftX: 35, bottomLeftY: 5, width: 20, height: 20)
+        
+        // Draw the mouth
+        canvas.drawEllipse(centreX: 20, centreY: -25, width: 80, height: 20)
+        
+        // Make mouth proper size
+        canvas.fillColor = Color.orange
+        canvas.drawEllipse(centreX: 20, centreY: -15, width: 80, height: 20)
+        
+        
+        
+        
         
         // Now "cut out" the wings and head by overlapping circles
         canvas.fillColor = Color.white
         
-        // Underside of wings
-        canvas.drawEllipse(centreX: -70, centreY: -10, width: 50, height: 50) // left
-        canvas.drawEllipse(centreX: -35, centreY: -10, width: 50, height: 50) // left middle
-        canvas.drawEllipse(centreX: 35, centreY: -10, width: 50, height: 50) // right middle
-        canvas.drawEllipse(centreX: 70, centreY: -10, width: 50, height: 50) // right
         
-        // Further down
-        canvas.drawEllipse(centreX: 0, centreY: -50, width: 50, height: 100) // middle
-        
-        // Get rid of rest further down
-        canvas.drawRectangle(centreX: 0, centreY: -35, width: 200, height: 30)
-        
-        // Now add the head
-        canvas.fillColor = Color.black
-        var headVertices : [NSPoint] = []
-        headVertices.append(NSPoint(x: -40, y: 40))
-        headVertices.append(NSPoint(x: 40, y: 40))
-        headVertices.append(NSPoint(x: 30, y: 80))
-        headVertices.append(NSPoint(x: 20, y: 60))
-        headVertices.append(NSPoint(x: -20, y: 60))
-        headVertices.append(NSPoint(x: -30, y: 80))
-        headVertices.append(NSPoint(x: -40, y: 40)) // end where we started
-        canvas.drawCustomShape(with: headVertices)
+
     }
     
 }
